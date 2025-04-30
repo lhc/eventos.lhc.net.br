526d4eb98586a7d095ebcb5b59ae53272d108bc1
Renne Rocha <renne@rocha.dev.br>
2025-04-30 20:35:58 -0300

    Fix command to add exported token

--- local_backup.sh
+++ local_backup.sh
@@ -1,5 +1,5 @@
 # Needed to start container in fly.io if it was stopped by inactivity
 curl -s -o /dev/null https://eventos.lhc.net.br
 
-fly ssh console -C 'sh remote_backup.sh'
-fly ssh sftp get "/root/gancio_data_$(date +%F).tar.gz"
+fly ssh console -C 'sh remote_backup.sh' -t $FLY_SSH_TOKEN
+fly ssh sftp get "/root/gancio_data_$(date +%F).tar.gz" -t $FLY_SSH_TOKEN
